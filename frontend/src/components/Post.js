6b289f8782f0c2c1b48d014dddb4b872560508d7
jzProg <john_z94@hotmail.com>
2020-12-20 22:37:24 +0200

    refactor: post date display UI improvement

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -8,7 +8,12 @@ function Post({ post, fullDate, onSelect }) {
     <div className='postDiv'>
       <span style={{ cursor: 'pointer', color: '#2196F3' }} onClick={onSelect}><b>{ post.title }</b></span>
       <span style={{ fontSize: '80%'}}>
-        <i> by <span style={{ color: 'orange' }}>{ post.author }</span>, { fullDate ? (<Moment format="MMMM Do YYYY, h:mm:ss a">{post.timestamp}</Moment>) : (<Moment fromNow>{post.timestamp}</Moment>) }</i>
+        <i> by <span style={{ color: 'orange' }}>{ post.author }</span>
+         <br/>
+         <span style={{ color: 'gray', fontSize: '80%'}}>
+          { fullDate ? (<Moment format="MMMM Do YYYY, h:mm:ss a">{post.timestamp}</Moment>) : (<Moment fromNow>{post.timestamp}</Moment>) }
+         </span>
+        </i>
       </span>
       <br/>
       <span style={{ fontSize: '80%'}} className='commentDiv'>{ post.commentCount} comments | { post.voteScore} votes</span>
