584756b42277396ed700e507979180b46490aab5
jzProg <john_z94@hotmail.com>
2020-12-20 03:12:38 +0200

    feat: post timestamp added

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Moment from 'react-moment';
 
 function Post({ post }) {
 
   return (
     <div>
       <h2> { post.title } </h2>
-      <h4><i> by { post.author }</i></h4>
+      <h4><i> by <span style={{ color: 'red' }}>{ post.author }</span>, <Moment fromNow>{post.timestamp}</Moment></i></h4>
       <div className='commentDiv'>{ post.commentCount} comments</div>
       <div className='scoreDiv'>{ post.voteScore} votes</div>
     </div>
