0780a277e1c7750bf2660730bc7f1d8614406c19
jzProg <john_z94@hotmail.com>
2020-12-20 21:21:16 +0200

    feat: display comment votes

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -9,6 +9,7 @@ function Comment({ commentInfo }) {
        <h4><span style={{ color: 'orange' }}>{ commentInfo.author }</span> says: </h4>
        <span><i style={{ color: 'gray', fontSize: '60%' }}><Moment format="MMMM Do YYYY, h:mm:ss a">{ commentInfo.timestamp }</Moment></i></span>
        <div>{ commentInfo.body }</div>
+       <span style={{ fontSize: '80%'}} className='commentDiv'>{ commentInfo.voteScore} votes</span>
        <EditControl/>
       </div>
     )
